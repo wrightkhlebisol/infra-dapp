208d21e91d7297720471936792c7f68dce2bbf2e
Wright <calebogundiya@gmail.com>
2020-06-26 23:57:09 +0100

    Ganache connection instead of Infura

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 let web3 = require('web3');
 
-let infuraUrl = "https://ropsten.infura.io/v3/304ed06f11c348e2b05c1dbc9e6a2c38";
-let walletAddress = "0x635B4764D1939DfAcD3a8014726159abC277BecC";
+let infuraUrl = "https://127.0.0.1:8545";
+let walletAddress = "0xcC5f5adCD9984ba32e41E870dEF8d9d761b0b988";
 let infuraConnect = new web3(infuraUrl);
 
-infuraConnect.eth.getBalance(walletAddress).then(console.log);
+console.log(infuraConnect.eth);
+
+// infuraConnect.eth.getBalance(walletAddress).then(console.log);
